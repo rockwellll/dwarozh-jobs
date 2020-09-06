68a8ff9c86bc4b6585b24ad0132402bc383df730
A7madXatab <Ahmadzendane725@gmail.com>
2020-09-06 05:28:59 -0700

    adding the select location input partial

---
+++ resources/views/partials/locations-select-input.blade.php
@@ -0,0 +1,11 @@
+<select required name="location" id="location"
+        class="bg-body @error('location') border-red-500 @enderror">
+    <option disabled selected value class="text-gray-500"> {{__('auth.select_location')}} </option>
+    @foreach(array_keys(__('auth.locations')) as $l)
+        <option @if(old('location') == $l) selected
+                @endif value="{{$l}}"
+        >
+            {{__('auth.locations')[$l]}}
+        </option>
+    @endforeach
+</select>
