1c78dbcc27096bcb0c147e9effc6d93c165742d1
A7madXatab <Ahmadzendane725@gmail.com>
2020-09-08 05:09:18 -0700

    extracting the change language links to a re-usable component

---
+++ resources/views/partials/language-change-links.blade.php
@@ -0,0 +1,7 @@
+@if(App::getLocale() == 'ku')
+    <a class="link accent padded-underline"
+       href="{{route($route, ['locale' => 'en'])}}">{{__('auth.view_in_another_language')}}</a>
+@else
+    <a class="link accent padded-underline"
+       href="{{route($route, ['locale' => 'ku'])}}">{{__('auth.view_in_another_language')}}</a>
+@endif
