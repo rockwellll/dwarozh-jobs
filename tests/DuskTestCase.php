b3f376d3d16710def00d9e347363fab994864ef1
Ahmad khatab <38360603+A7madXatab@users.noreply.github.com>
2020-09-12 01:16:00 -0700

    Update DuskTestCase.php

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -32,7 +32,8 @@ abstract class DuskTestCase extends BaseTestCase
         $options = (new ChromeOptions)->addArguments([
             '--disable-gpu',
             '--headless',
-            '--window-size=1920,1080'
+            '--window-size=1920,1080',
+            '--no-sandbox'
         ]);
 
         return RemoteWebDriver::create(
