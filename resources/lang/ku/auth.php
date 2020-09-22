e30449f34768ca0fdbb40ed229be4890a978e76f
A7madXatab <Ahmadzendane725@gmail.com>
2020-09-22 00:13:16 -0700

    latest code

--- resources/lang/ku/auth.php
+++ resources/lang/ku/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'وشه‌ی نهێنی یان ئیمه‌یڵ هه‌ڵه‌یه‌.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
     'login_to_account' => "چوونه‌ژووره‌وه‌ بۆ ناو هه‌ژماره‌که‌ت",
