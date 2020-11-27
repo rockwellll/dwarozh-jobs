7d7233b4a00ef1abf36a2a55bb06dcf42a699c85
A7madXatab <Ahmadzendane725@gmail.com>
2020-11-27 21:14:10 +0300

    adding tests to validate toggle favorite button livewire

--- tests/Feature/ToggleFavoriteButtonTest.php
+++ tests/Feature/ToggleFavoriteButtonTest.php
@@ -1,20 +1,53 @@
 <?php
-//
-//use App\Models\Job;
-//use Illuminate\Support\Facades\Artisan;
-//use function Pest\Livewire\livewire;
-//use function  Pest\Laravel\actingAs;
-//
-//use App\Models\User;
-//use App\Models\BusinessUser;
-//
-//it('the toggle button will be visible for authenticated users', function () {
-//    Artisan::call('db:seed');
-//    $account = BusinessUser::create();
-//
-//    $account->jobs()->save(Job::factory()->create());
-//
-//
-//    actingAs(User::factory()->create())
-//        ->get('/jobs')        ->assertSeeLivewire('favorite-button');
-//});
+
+
+use App\Models\BusinessUser;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
+use function PHPUnit\Framework\assertEquals;
+
+function data()
+{
+    Artisan::call('db:seed');
+    $account = BusinessUser::create();
+
+    $account->jobs()->save(Job::factory()->create());
+
+    $user = User::factory()->create();
+    $u = \App\Models\DefaultUser::create();
+
+    $u->user()->save($user);
+
+    return $user;
+}
+
+it('the toggle button will be visible for authenticated users who are not business users', function () {
+    $user = data();
+    actingAs($user)
+        ->get('/en/jobs')
+        ->assertSeeLivewire('favorite-button');
+});
+
+it('a user can toggle the favorite status for a posted job', function () {
+    $user = data();
+    \Livewire\Livewire::actingAs($user);
+    $job = Job::first();
+
+    assertEquals(0, count($user->userable->favorites(Job::class)->get()));
+
+    livewire(\App\Http\Livewire\FavoriteButton::class, ['job' => $job])
+        ->assertSet('isFavorited', false)
+        ->call('submit')
+        ->assertSet('isFavorited', true);
+
+    assertEquals(1, count($user->userable->favorites(Job::class)->get()));
+
+    livewire(\App\Http\Livewire\FavoriteButton::class, ['job' => $job])
+        ->assertSet('isFavorited', true)
+        ->call('submit')
+        ->assertSet('isFavorited', false);
+
+});
